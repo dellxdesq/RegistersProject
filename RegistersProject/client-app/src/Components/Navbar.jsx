4e0cac3fbfbca08aa39e5c7c31d247c0f3f5690c
dellxdesq <nikita673551@gmail.com>
2025-04-13 23:21:46 +0300

    Fix: добавлен переход на главную и личный кабинет в навбаре

--- RegistersProject/client-app/src/Components/Navbar.jsx
+++ RegistersProject/client-app/src/Components/Navbar.jsx
@@ -1,10 +1,17 @@
 import styles from "../Pages/Main/styles";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
+
     return (
         <div style={styles.navbar}>
-            <button style={styles.navButton}>Главная</button>
-            <button style={styles.navButton}>Личный кабинет</button>
+            <button style={styles.navButton} onClick={() => navigate("/")}>
+                Главная
+            </button>
+            <button onClick={() => navigate("/personal")} style={styles.navButton}>
+                Личный кабинет
+            </button>
         </div>
     );
 }
